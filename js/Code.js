0a0e5e7b1e6bdc9889ae73270352a250c72d3e1e
Kworqz <90353659+Kworqz@users.noreply.github.com>
2022-10-28 16:12:07 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,18 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "DATABASE";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+  // Adicione código para exibir as outras duas perguntas.
+  textSize(15)
+  text("Dica: Realiza uma tarefa específica!",700,180);
+
+  text("Dica: Guarda todas as informações!",100,375)
+
+
+}
